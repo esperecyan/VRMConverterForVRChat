4c5d47c57a6753ae431b7731b08a62ac9dffbdab
100の人 <100@pokemori.jp>
2018-12-12 22:19:32 +0900

    Unityのバージョンに関する注意メッセージが国際化できていなかったバグを修正

--- Editor/Wizard.cs
+++ Editor/Wizard.cs
@@ -170,7 +170,7 @@ namespace Esperecyan.Unity.VRMConverterForVRChat
             if (Application.unityVersion != VRChatUtility.SupportedUnityVersion)
             {
                 EditorGUILayout.HelpBox(string.Format(
-                    "Unity {0} is running. If you are using a different version than {1}, VRChat SDK might not work correctly. Recommended using Unity downloaded from {2} .",
+                    Gettext._("Unity {0} is running. If you are using a different version than {1}, VRChat SDK might not work correctly. Recommended using Unity downloaded from {2} ."),
                     Application.unityVersion,
                     VRChatUtility.SupportedUnityVersion,
                     VRChatUtility.DownloadURL
