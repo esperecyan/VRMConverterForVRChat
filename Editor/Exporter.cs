0677864337137b20637ba6dd5e211a46065d2c5a
100の人 <100@pokemori.jp>
2018-12-28 18:29:34 +0900

    Revert "UniVRM v0.46c が v0.46 扱いになる問題に対処"
    This reverts commit 0a4616efddbda63e9b936ab2bb5442ae6969735c.
    
    UniVRM v0.47がリリースされたため。

--- Editor/Exporter.cs
+++ Editor/Exporter.cs
@@ -27,7 +27,7 @@ namespace Esperecyan.Unity.VRMConverterForVRChat
                 string name = Exporter.PackageName;
 
                 if (withUniVRM) {
-                    name += " + " + VRMVersion.VRM_VERSION + (VRMVersion.VERSION == "0.46" ? "c" : "");
+                    name += " + " + VRMVersion.VRM_VERSION;
                     assetPathNames = assetPathNames.Concat(allAssetPathNames.Where(path => path.StartsWith("Assets/VRM/")));
                 }
 
