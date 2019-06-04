a4855f214a64bb9b33602e903baa2bdc1f42068c
100の人 <100@pokemori.jp>
2019-06-04 21:53:43 +0900

    v7.3.0

--- Editor/Converter.cs
+++ Editor/Converter.cs
@@ -32,7 +32,7 @@ namespace Esperecyan.Unity.VRMConverterForVRChat
         /// <summary>
         /// 当エディタ拡張のバージョン。
         /// </summary>
-        public static readonly string Version = "7.2.1";
+        public static readonly string Version = "7.3.0";
 
         /// <summary>
         /// プレハブをVRChatへアップロード可能な状態にします。
